0ba637f7fd367d66dd84c734a6d943512392f91d
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-08 22:04:49 +0700

    membuat GET, POST, PUT, DELETE request dengan handling error dan bug fixing

--- crud.js
+++ crud.js
@@ -11,31 +11,66 @@ const siswa = [
   { id: 3, nama: 'Karto Tuying' }
 ];
 
+// GET request
 app.get('/api/siswa', (req, res) => {
   res.send(siswa);
 });
 
+// POST request
+app.post('/api/siswa', (req, res) => {
+  // handling error 400 bad request :  *ini adalah step 2
+  const {error} = validasiSiswa(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // ini adalah step 1 dari POST request
+  const dataSiswa = {
+    id  : siswa.length + 1,
+    nama: req.body.nama
+  };
+  siswa.push(dataSiswa);
+  res.send(dataSiswa);
+});
+
+// PUT request
+app.put('/api/siswa/:id', (req, res) => {
+  // handling error 404 not found *step 2
+  const dataSiswa = siswa.find(i => i.id === parseInt(req.params.id));
+  if (!dataSiswa) return res.status(404).send('Data siswa dengan id ini tidak ditemukan');
+
+  // handling error 400 *step 3
+  const {error} = validasiSiswa(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // *step 1 PUT request
+  dataSiswa.nama = req.body.nama;
+  res.send(dataSiswa);
+
+});
+
+// DELETE request
+app.delete('/api/siswa/:id', (req, res) => {
+  // handling error 404 not found *step 2
+  const dataSiswa = siswa.find(i => i.id === parseInt(req.params.id));
+  if (!dataSiswa) return res.status(404).send('Data siswa dengan id ini tidak ditemukan');
+
+  const index = siswa.indexOf(dataSiswa);
+  siswa.splice(index, 1);
+
+  res.send(siswa);
+})
 
 
 
 
+// ____________________________________________________________________//
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// VALIDASI REQUEST
+function validasiSiswa(dataSiswa) {
+  const skema = {
+    nama: Joi.string().min(3).required()
+  };
+  return Joi.validate(dataSiswa, skema);
+}
 
 // PORT
 const port = process.env.port || 3500;
